934f19cdd44616b6fa1097533afcba16d77c7491
angelchavez19 <infoangelchavez@gmail.com>
2025-09-26 09:21:01 -0500

    feat: reset command

--- codenust-os/src/main.rs
+++ codenust-os/src/main.rs
@@ -20,16 +20,6 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-fn print_banner() {
-    println!("   _____          _                      _    ____   _____ ");
-    println!("  / ____|        | |                    | |  / __ \\ / ____|");
-    println!(" | |     ___   __| | ___ _ __  _   _ ___| |_| |  | | (___  ");
-    println!(" | |    / _ \\ / _` |/ _ \\ '_ \\| | | / __| __| |  | |\\___ \\ ");
-    println!(" | |___| (_) | (_| |  __/ | | | |_| \\__ \\ |_| |__| |____) |");
-    println!("  \\_____\\___/ \\__,_|\\___|_| |_|\\__,_|___/\\__|\\____/|_____/ ");
-    println!();
-}
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     unsafe {
@@ -50,7 +40,7 @@ pub extern "C" fn _start() -> ! {
     }
 
     // 6) Probamos la salida por VGA
-    print_banner();
+    crate::programs::reset::print_banner();
     print!("> ");
 
     // Bucle infinito
--- codenust-os/src/programs/mod.rs
+++ codenust-os/src/programs/mod.rs
@@ -1,8 +1,10 @@
 pub mod clear;
 pub mod echo;
+pub mod reset;
 
 use crate::programs::clear::clear_program;
 use crate::programs::echo::echo_program;
+use crate::programs::reset::reset_command;
 
 pub struct Program {
     pub name: &'static str,
@@ -17,5 +19,9 @@ pub static PROGRAMS: &[Program] = &[
     Program {
         name: "clear",
         entry: clear_program,
+    },
+    Program {
+        name: "reset",
+        entry: reset_command,
     },
 ];
